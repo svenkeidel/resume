84c80ee3cc1e5dcae94bac39f726fd643f830b77
Sven Keidel <svenkeidel@pm.me>
2023-05-11 16:38:45 +0200

    update

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -6,6 +6,9 @@ const sh = require('shelljs');
 module.exports = function renderAssets() {
     const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
     const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
-    
+
     sh.cp('-R', sourcePath, destPath)
+
+    const sourceKeybase = upath.resolve(upath.dirname(__filename), '../src/keybase.txt')
+    sh.cp(sourceKeybase, destPath)
 };
